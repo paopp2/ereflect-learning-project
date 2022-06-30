311688e488e822ce05bed8b3d0c00828644b480d
Paolo Pepito <paolo@ereflect.com>
2022-06-30 11:52:27 +0800

    Generate files for TextTypeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TextSourceComponent } from './text-type/text-source/text-source.component';
 import { TextInputComponent } from './text-type/text-input/text-input.component';
+import { TextTypeComponent } from './text-type/text-type.component';
 
 declare var require: any;
 
 @NgModule({
   declarations: [
     AppComponent,
+    TextTypeComponent,
     TextSourceComponent,
     TextInputComponent,
   ],
---
+++ src/app/text-type/text-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-typ',
+  templateUrl: './text-type.component.html',
+  styleUrls: ['./text-type.component.css']
+})
+export class TextTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
