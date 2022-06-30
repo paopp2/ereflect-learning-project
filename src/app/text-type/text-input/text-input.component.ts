b9e30e8861f53b31ab60d59daf9296990e6a5d1b
Paolo Pepito <paolo@ereflect.com>
2022-06-30 14:49:01 +0800

    Remove unused code

--- src/app/text-type/text-input/text-input.component.ts
+++ src/app/text-type/text-input/text-input.component.ts
@@ -13,25 +13,14 @@ export class TextInputComponent {
   typedText: string = '';
   @Output() typedTextChange = new EventEmitter<string[]>();
 
-  resetEventSubscription: Subscription;
-  // inputTries: number = 0;
-  charactersTyped: number = 0;
-
   constructor(private typingHelperService: TypingHelperService) {
-    this.resetEventSubscription = this.typingHelperService.getResetEvent().subscribe(() => { 
+    this.typingHelperService.getResetEvent().subscribe(() => { 
       // Clear typed text
       this.typedText = "";
       this.typedTextChange.emit([]);
     });
   }
 
-  // startTimer() {
-  //   this.inputTries++;
-  //   if (this.inputTries <= 1) {
-  //     this.typingHelperService.startEvent();
-  //   }
-  // }
-  
   onInput(input: string) {
     this.typedTextChange.emit(input.split(''));
   }
