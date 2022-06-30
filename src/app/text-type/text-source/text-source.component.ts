53149272158eaa00c5451e2573b9321ddb0491b0
Paolo Pepito <paolo@ereflect.com>
2022-06-30 14:05:13 +0800

    Minor refactors

--- src/app/text-type/text-source/text-source.component.ts
+++ src/app/text-type/text-source/text-source.component.ts
@@ -11,11 +11,10 @@ const txtgen = require('txtgen');
 
 export class TextSourceComponent implements OnInit {
   @Input() textInputArray: string[] = [];
-  charactersArr: string[] = [];
-  resetEventSubscription: Subscription;
+  charactersArray: string[] = [];
 
   constructor(private typingHelperService: TypingHelperService) {
-    this.resetEventSubscription = this.typingHelperService.getResetEvent().subscribe(() => this.setSentence());
+    this.typingHelperService.getResetEvent().subscribe(() => this.setSentence());
   }
 
   ngOnInit(): void {
@@ -26,16 +25,16 @@ export class TextSourceComponent implements OnInit {
     let text: string = '';
 
     // Find text that fits length requirements
-    while (!(text.length >= 300) && (text.length <= 340)) { 
-      text = txtgen.paragraph(); 
+    while (!(text.length >= 300) && (text.length <= 340)) {
+      text = txtgen.paragraph();
     }
 
-    this.charactersArr = text.split('');
+    this.charactersArray = text.split('');
   }
-  
-  // Returns CSS class based on how arguments match:
-  compareLetters(requiredLetter: string, typedLetter?: string, ): string {
-    if(!typedLetter) return 'untyped';
+
+  // Returns CSS class based on how arguments match
+  compareLetters(requiredLetter: string, typedLetter?: string,): string {
+    if (!typedLetter) return 'untyped';
     return (typedLetter === requiredLetter) ? 'correct' : 'wrong';
   }
 }
