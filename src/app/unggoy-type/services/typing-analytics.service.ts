abb72030c3e01feda4268f27fcdae74f84fbafc2
Paolo Pepito <paolo@ereflect.com>
2022-07-04 09:14:35 +0800

    WIP: Move some logic from StatsComponent to service
    Some of that logic can be used for computing typing stats

--- src/app/unggoy-type/services/typing-analytics.service.ts
+++ src/app/unggoy-type/services/typing-analytics.service.ts
@@ -6,11 +6,14 @@ import { DOCUMENT } from '@angular/common';
 })
 export class TypingAnalyticsService {
   inputText: string = '';
-  displayText: string = ''; 
+  displayText: string = '';
   phraseTextArray: Array<HTMLSpanElement> = [];
   errorCount: number = 0;
+  storeDs: number = 0;
+  running: boolean = false;
+  interval: any;
 
-  constructor(@Inject(DOCUMENT) private _document: any){}
+  constructor(@Inject(DOCUMENT) private _document: any) { }
   // Signals to subscribe to
   public startSubject = new Subject<void>();
   public resetSubject = new Subject<void>();
@@ -18,37 +21,57 @@ export class TypingAnalyticsService {
   private errorSubject = new Subject<number>();
 
   errorData = this.errorSubject.asObservable();
-  
+
   get inputTextArr(): string[] { return this.inputText.split('') }
   get displayTextArr(): string[] { return this.displayText.split('') }
 
   start() { this.startSubject.next(); }
 
-  reset() { 
-    this.resetSubject.next(); 
+  reset() {
+    this.resetSubject.next();
     this.inputText = '';
     this.displayText = '';
     this.errorSubject.next(0);
   }
 
+  timer() {
+    if (this.running) {
+      this.storeDs++;
+    }
+  }
+
+
   stop() { this.stopSubject.next(); }
-  
+
+
   stopOnFinish() {
-    if(this.inputText.length === this.displayText.length) {
+    if (this.inputText.length === this.displayText.length) {
       this.stop();
     }
   }
 
   errorChecker() {
     let errorsMade: number = 0;
-    this.phraseTextArray = this._document.getElementById('phraseContainer').querySelectorAll('span'); 
+    this.phraseTextArray = this._document.getElementById('phraseContainer').querySelectorAll('span');
 
     this.phraseTextArray.forEach((currentCharacter: HTMLSpanElement, currentIndex: number) => {
-      if (currentCharacter.innerText !== this.inputText[currentIndex] && this.inputText[currentIndex] != null){
+      if (currentCharacter.innerText !== this.inputText[currentIndex] && this.inputText[currentIndex] != null) {
         errorsMade++;
       }
     });
 
     this.errorSubject.next(errorsMade);
   }
+
+  // onStart() {
+  //   this.running = true;
+  //   this.interval = setInterval(() => this.timer(), 10); // Update every decisecond
+  // }
+  
+  // onReset() {
+  //   this.analyticsService.reset(); //resets the whole thing
+  //   clearInterval(this.interval); //clears the interval in setInterval above
+  //   this.storeDs = 0;
+  // }
+
 }
--- src/app/unggoy-type/stats/stats.component.ts
+++ src/app/unggoy-type/stats/stats.component.ts
@@ -6,35 +6,16 @@ import { TypingAnalyticsService } from '../services/typing-analytics.service';
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent implements OnInit {
-  interval: any;
-  running: boolean = false;
-  storeDs: number = 0;
   errorCounter: number = 0;
 
   constructor(public analyticsService: TypingAnalyticsService) { 
-    this.analyticsService.startSubject.subscribe(() => this.onStart());
-    this.analyticsService.stopSubject.subscribe(() => this.running = false);
+    this.analyticsService.startSubject.subscribe(() => {});
+    this.analyticsService.stopSubject.subscribe(() => {});
     this.analyticsService.errorData.subscribe(data => {  //subscribe to error counter from the service
       this.errorCounter = data;
     })
   }
 
   ngOnInit(): void { }
-
-  onStart() {
-    this.running = true;
-    this.interval = setInterval(() => this.timer(), 10); // Update every decisecond
-  }
-  
-  timer() {
-    if (this.running) {
-      this.storeDs++;
-    }
-  }
-  onReset() {
-    this.analyticsService.reset(); //resets the whole thing
-    clearInterval(this.interval); //clears the interval in setInterval above
-    this.storeDs = 0;
-  }
 
 }
