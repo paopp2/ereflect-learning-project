7d9d3265428bbcd867fefc95b6b9117070f6b866
Paolo Pepito <paolo@ereflect.com>
2022-07-04 11:42:31 +0800

    Remove spec file for time-display pipe

--- src/app/unggoy-type/pipes/time-display.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeDisplayPipe } from './time-display.pipe';
-
-describe('TimeDisplayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeDisplayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
