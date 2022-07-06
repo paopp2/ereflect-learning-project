6ceb4a9fab98b2db4146103e68353228dfa06fda
Paolo Pepito <paolo@ereflect.com>
2022-07-07 07:54:10 +0800

    Temporarily modified routes at AppRouting module for auth testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { UnggoyTypeComponent } from './unggoy-type/unggoy-type.component';
 import { AboutComponent } from './about/about.component';
 import { AuthComponent } from './auth/auth/auth.component';
+
+// TODO: Revert routes to original. Temporarily done like this for testing auth feature
 const routes: Routes = [
-    { path: '', component: UnggoyTypeComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'home', component: UnggoyTypeComponent },
     { path: 'login', component: AuthComponent },
     { path: 'about', component: AboutComponent },
 ]; // sets up routes constant where you define your routes
