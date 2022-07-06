b96537de2c1fbc67013bb353a077a68b51eb55e9
Monica <monicabee358@gmail.com>
2022-07-06 14:47:48 +0800

    Redirect to landing when UnggoyType is clicked

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -15,5 +16,4 @@ export class HeaderComponent implements OnInit {
   goGithub() {
     window.location.href='https://github.com/paopp2/ereflect-learning-project';
   }
-
 }
