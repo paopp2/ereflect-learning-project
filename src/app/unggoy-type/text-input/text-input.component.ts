0638e5a7534f3df2fb1f6004ae85a357f3b0c023
Monica <monicabee358@gmail.com>
2022-07-06 08:18:29 +0800

    removed horizontal and vertical configs

--- src/app/unggoy-type/text-input/text-input.component.ts
+++ src/app/unggoy-type/text-input/text-input.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import {
   MatSnackBar,
-  MatSnackBarHorizontalPosition,
-  MatSnackBarVerticalPosition,
 } from '@angular/material/snack-bar';
 import { MatSnackBarConfig } from '@angular/material/snack-bar';
 import { Subscription } from 'rxjs';
@@ -17,16 +15,14 @@ import { TypingStatsService } from '../services/typing-stats.service';
 export class TextInputComponent implements OnDestroy {
   @Output() inputChange = new EventEmitter<InputData>();
   isTypeFinished = false;
-  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
-  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
   private resetSubscription: Subscription;
   private stopSubscription: Subscription;
 
   private configSuccess: MatSnackBarConfig = {
     duration: 2000,
     panelClass: ['style-success'],    
-    horizontalPosition: this.horizontalPosition,
-    verticalPosition: this.verticalPosition,
+    horizontalPosition:'start',
+    verticalPosition: 'bottom',
   };
   
   constructor(public statsService: TypingStatsService, private snackbar: MatSnackBar) {
