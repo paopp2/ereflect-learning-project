9602a0beba5e87e8b3f7b64143b43efb0552b171
Monica <monicabee358@gmail.com>
2022-07-08 15:57:02 +0800

    removed unused imports

--- src/app/contact-us/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us/contact-us.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
 import { EmailService } from './services/email.service';
 
