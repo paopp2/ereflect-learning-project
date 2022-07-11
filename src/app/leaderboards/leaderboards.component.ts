7de94a2994c3b73d7cef00ae29b02bcfc8f6715d
Paolo Pepito <paolo@ereflect.com>
2022-07-11 09:27:28 +0800

    MergeMap topUserStats$ that depends on the route.queryMap$ observable

--- src/app/leaderboards/leaderboards.component.ts
+++ src/app/leaderboards/leaderboards.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { mergeMap, Subscription } from 'rxjs';
 import { UserStats } from '../models/user-stats.model';
 import { UserStatsRepoService } from '../user-stats-repo/user-stats-repo.service';
 
@@ -8,9 +9,9 @@ import { UserStatsRepoService } from '../user-stats-repo/user-stats-repo.service
   templateUrl: './leaderboards.component.html',
   styleUrls: ['./leaderboards.component.css']
 })
-export class LeaderboardsComponent implements OnInit {
-  statsType: string = 'wpm';
-  topUserStats: UserStats[]  = [];
+export class LeaderboardsComponent implements OnInit, OnDestroy {
+  topUserStats: UserStats[] = [];
+  private userStatsSub!: Subscription;
 
   constructor(
     private userStatsRepo: UserStatsRepoService,
@@ -18,18 +19,15 @@ export class LeaderboardsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe({
-      next: (params) => {
-        console.log(params);
-        this.statsType = params['stats'] ?? 'wpm';
-      },
-    });
-
-    this.userStatsRepo.getTopUserStats(this.statsType).subscribe({
-      next: (userStats) => this.topUserStats = userStats,
-    });
+    this.userStatsSub = this.route.queryParams.pipe(
+      mergeMap(params => this.userStatsRepo.getTopUserStats(params['stats'] ?? 'wpm')),
+    ).subscribe(userStats => this.topUserStats = userStats);
   }
   
+  ngOnDestroy(): void {
+    this.userStatsSub.unsubscribe();
+  }
+
   userStatsById(index: number, userStats: UserStats) {
     return userStats.user.id;
   }
