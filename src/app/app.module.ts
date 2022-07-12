0db3eb17f56f1c0203087cd17273d0abeedf0212
Paolo Pepito <paolo@ereflect.com>
2022-07-12 20:05:18 +0800

    Move core dependencies to CoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { ContactUsModule } from './features/contact-us/contact-us.module';
@@ -16,20 +8,10 @@ import { AuthModule } from './features/auth/auth.module';
 import { LeaderboardsModule } from './features/leaderboards/leaderboards.module';
 import { HomeModule } from './features/home/home.module';
 @NgModule({
-  declarations: [
-    AppComponent,
- 
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    NoopAnimationsModule,
-    AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    BrowserAnimationsModule,
-    SharedModule,
     CoreModule,
+    SharedModule,
     AboutModule,
     ContactUsModule,
     AuthModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
+    BrowserModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
-  exports: [],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    BrowserModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
 })
 export class CoreModule { }
